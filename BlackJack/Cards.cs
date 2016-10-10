e07bf513c543171574e1198ad7962bc872b30ac4
Curtis Browning <clb12369@yahoo.com>
2016-10-09 22:29:09 -0500

    initial commit

---
+++ BlackJack/Cards.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace BlackJack
+{
+    public enum Suit
+    {
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
+    }
+
+    public enum Rank
+    {
+        Ace,
+        Deuce,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public class Card
+    {
+        public Suit suit;
+        public Rank rank;
+
+        public Card(Suit s, Rank r)
+        {
+            suit = s;
+            rank = r;
+        }
+
+        public override string ToString()
+        {
+            return rank.ToString() + " of " + suit.ToString();
+        }
+
+
+    }
+
+    public class Deck
+    {
+        public Card[] cards;
+        public int numberDealt = 0;
+
+        public Deck()
+        {
+            cards = new Card[52];
+            int i = 0; // 1
+            foreach (Rank r in Enum.GetValues(typeof(Rank)))
+            {
+                int j = 0;
+                foreach (Suit s in Enum.GetValues(typeof(Suit)))
+                {
+                    cards[i * 4 + j] = new Card(s, r);
+                    j++;
+                }
+                i++;
+            }
+        }
+
+
+        public Deck ShuffleDeck()
+        {
+            Random randomCard = new Random();
+            for (int i = 0; i < 52; i++)
+            {
+                var temp = cards[i];
+                int cardIndex = randomCard.Next(51);
+                cards[i] = cards[cardIndex];
+                cards[cardIndex] = temp;
+            }
+            return this;
+        }
+
+        public Card deal() => (numberDealt < cards.Length) ? cards[numberDealt++] : null;
+
+
+        public Card DealFromDeck()
+        {
+            if (numberDealt < cards.Length)
+            {
+                return cards[numberDealt++];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public class Hand
+    {
+        public int score { get; private set; } = 0;
+        public Card[] hand = new Card[10];
+        public int handPosition {get; private set;} = 0;
+
+        public void TakeCard(Card newCard)
+        {
+            hand[handPosition] = newCard;
+            handPosition++;
+        }
+
+        public void CalculateScore() {
+            score = 0;
+            for(int i = 0; i < handPosition; i++)
+            {
+                switch (hand[i].rank)
+                {
+                    case Rank.Ace:
+                        score += 11;
+                        break;
+                    case Rank.Deuce:
+                        score += 2;
+                        break;
+                    case Rank.Three:
+                        score += 3;
+                        break;
+                    case Rank.Four:
+                        score += 4;
+                        break;
+                    case Rank.Five:
+                        score += 5;
+                        break;
+                    case Rank.Six:
+                        score += 6;
+                        break;
+                    case Rank.Seven:
+                        score += 7;
+                        break;
+                    case Rank.Eight:
+                        score += 8;
+                        break;
+                    case Rank.Nine:
+                        score += 9;
+                        break;
+                    case Rank.Ten:
+                        score += 10;
+                        break;
+                    case Rank.Jack:
+                        score += 10;
+                        break;
+                    case Rank.Queen:
+                        score += 10;
+                        break;
+                    case Rank.King:
+                        score += 10;
+                        break;
+                    default:
+                        score += 0;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ BlackJack/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace BlackJack
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Would you like to play BlackJack (Y/N)?");
+            string answer = Console.ReadLine().ToUpper();
+            if (answer == "Y")
+            {
+                Play();
+            } else
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        static void Play()
+        {
+            Hand playerHand = new Hand();
+            Hand dealerHand = new Hand();
+
+            Deck dealerDeck = new Deck().ShuffleDeck();
+            for (int i = 0; i < 2; i++)
+            {
+                playerHand.TakeCard(dealerDeck.DealFromDeck());
+                dealerHand.TakeCard(dealerDeck.DealFromDeck());
+            }
+            Console.WriteLine("Players hand:");
+            for (int i = 0; i <= playerHand.handPosition; i++)
+            {
+                Console.WriteLine(playerHand.hand[i]);
+            }
+            playerHand.CalculateScore();
+            Console.WriteLine("Player score: {0}", playerHand.score);
+            if (playerHand.score == 21)
+            {
+                Console.WriteLine("BlackJack!! you're the winner");
+                AskToPlayAgain();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dealer's hand:");
+            Console.WriteLine(dealerHand.hand[0]);
+            dealerHand.CalculateScore();
+            Console.WriteLine();
+            //Console.WriteLine("Dealer score: {0}", dealerHand.score);
+
+            string playerAnswer;
+            do
+            {
+                Console.WriteLine("Would you like to hit or stay (H/S)?");
+                playerAnswer = Console.ReadLine().ToUpper();
+                if (playerAnswer == "H")
+                {
+                    Console.WriteLine("Player's hand:");
+                    playerHand.TakeCard(dealerDeck.DealFromDeck());
+                    Console.WriteLine(playerHand.hand[playerHand.handPosition - 1]);
+                    playerHand.CalculateScore();
+                    Console.WriteLine("Player score: {0}", playerHand.score);
+                    if (playerHand.score > 21)
+                    {
+                        Console.WriteLine("You're busted!!");
+                        AskToPlayAgain();
+                    }
+                }
+            } while (playerAnswer == "H");
+
+            for (int i = 0; i <= playerHand.handPosition; i++)
+            {
+                Console.WriteLine(playerHand.hand[i]);
+            }
+            Console.WriteLine("Player score: {0}", playerHand.score);
+
+            if (playerHand.score > 21)
+            {
+                Console.WriteLine("You're busted!!");
+                AskToPlayAgain();
+            }
+
+            if (dealerHand.score < 16)
+            {
+                dealerHand.TakeCard(dealerDeck.DealFromDeck());
+                dealerHand.CalculateScore();
+                if (dealerHand.score < 16)
+                {
+                    dealerHand.TakeCard(dealerDeck.DealFromDeck());
+                    dealerHand.CalculateScore();
+                }
+                    if (dealerHand.score < 16)
+                    {
+                        dealerHand.TakeCard(dealerDeck.DealFromDeck());
+                        dealerHand.CalculateScore();
+                    }
+            }
+
+            dealerHand.CalculateScore();
+            Console.WriteLine("Dealer score {0}", dealerHand.score);
+
+            if (dealerHand.score > 21 && playerHand.score <= 21)
+            {
+                Console.WriteLine("Dealer busts! Player wins!");
+            } else if (playerHand.score > dealerHand.score)   
+            {
+                Console.WriteLine("Player wins!!");
+                AskToPlayAgain();
+
+            } else if (dealerHand.score > playerHand.score) {
+                Console.WriteLine("Dealer wins!!");
+                AskToPlayAgain();
+            } else if (playerHand.score > 21 && dealerHand.score > 21)
+            {
+                Console.WriteLine("It's a draw!!");
+                AskToPlayAgain();
+            } else if (dealerHand.score == playerHand.score)
+            {
+                Console.WriteLine("It's a tie...");
+                AskToPlayAgain();
+            }
+
+            Console.WriteLine("Goodbye");
+            Console.ReadLine();
+        }
+
+        static void AskToPlayAgain()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Would you like to play again (Y/N)?");
+            string answer = Console.ReadLine().ToUpper();
+            if (answer == "Y")
+            {
+                Play();
+            } else
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
